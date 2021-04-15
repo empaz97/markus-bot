f2bce31532d1d7007247bfcc91fae9bbad0d4ff1
Emily Pazienza <empaz97@gmail.com>
2021-04-14 23:58:49 -0400

    lets organize you better

--- constants/commands.js
+++ constants/commands.js
@@ -1,5 +1,19 @@
 const commands = {
-  HELP_COMMANDS: ["help me", "what can you do", "how do you work"],
-  GREETING_COMMANDS: ["hello", "hi there", "howdy"]
+  help: {
+    commands: ["help me", "what can you do", "how do you work"],
+    responses: [
+      "I'm a work in progress. Please contact Emerson if you need help"
+    ]
+  },
+  greeting: {
+    commands: ["hello", "hi there", "howdy"],
+    reaction: "👋",
+    responses: ["Oh hello!", "Hello, how are you!", "Hey there!"]
+  },
+  coffee: {
+    commands: ["get me a coffee", "make me a coffee", "bring me a coffee"],
+    responses: ["No thanks.", "You can get it yourself."],
+    niceResponses: ["Only because you asked nicely ☕"]
+  }
 };
 module.exports = commands;
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const Discord = require("discord.js");
-const commands = require("./constants/commands");
+
+const Markus = require("./markus");
 
 const bot = new Discord.Client();
 const token = "ODMxOTczNjA0NjEwNDA4NTA4.YHdBzA.ddWGxQbMCbnrrWREL3gCvVNZlHU";
-const name = "markus";
-const punctuationRegex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
 
 bot.on("ready", () => {
   console.log("bot is ready");
@@ -13,25 +12,7 @@ bot.on("ready", () => {
 bot.on("message", function(message) {
   if (message.author.bot) return;
 
-  const messageContent = message.content
-    .toLowerCase()
-    .replace(punctuationRegex, "");
-  if (!messageContent.includes(name)) {
-    return;
-  }
-
-  if (commands.HELP_COMMANDS.some(cmd => messageContent.includes(cmd))) {
-    message.channel.send(
-      "I'm a work in progress. If you have questions ask @LizardEm"
-    );
-  } else if (
-    commands.GREETING_COMMANDS.some(cmd => messageContent.includes(cmd))
-  ) {
-    message.react("👋");
-    message.reply("Hello, how are you!");
-  } else if (messageContent.includes("say something")) {
-    message.channel.send("Something.");
-  }
+  Markus.respondToMessage(message);
 });
 
 bot.login(token);
