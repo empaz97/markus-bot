c40d047c5e15d27ce6cbda71c1c13e954668e2c6
Emily Pazienza <empaz97@gmail.com>
2021-06-29 17:00:37 -0400

    Test mode

--- constants/base.js
+++ constants/base.js
@@ -1,7 +1,8 @@
 const baseConstants = {
   name: "markus",
   punctuationRegex: /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g,
-  mentionRegex: / <@!?(\d+)> /g
+  mentionRegex: / <@!?(\d+)> /g,
+  testServerIds: ["540252347306082314"]
 };
 
 module.exports = baseConstants;
